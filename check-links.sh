6abf197f62bb86222adb349dba8ca166346a3cf5
jrnold <jeffrey.arnold@gmail.com>
2016-01-04 12:32:25 -0800

    :bug: logic of check links was backwards

--- check-links.sh
+++ check-links.sh
@@ -4,9 +4,9 @@ nikola build > /dev/null
 bad_links=$(nikola check -l -r 2>&1 | grep "\(WARNING\|ERROR\):" )
 if [ -z "$bad_links" ]
 then
+    exit 0
+else
     echo "$bad_links"
     exit 1
-else
-    exit 0
 fi
 
