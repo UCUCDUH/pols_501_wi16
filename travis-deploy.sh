0361df9b850d7ca706945a23cfec31c009f9d671
jrnold <jeffrey.arnold@gmail.com>
2016-01-05 17:54:05 -0800

    deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,4 +7,4 @@ cd web/output
 git init
 git add .
 git commit -m "Travis update"
-git push -f "https://${GH_TOKEN}@github.com/${GH_REF}/" master:gh-pages 2>&1 | sed -e "s/${GH_TOKEN}/XXXX/g"
+git push -f "https://${GH_TOKEN}@github.com/${GH_REF}/" master:gh-pages 2>&1 
