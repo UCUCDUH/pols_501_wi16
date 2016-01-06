e5dba532cccf1f44295a92713fc80e70819b3416
jrnold <jeffrey.arnold@gmail.com>
2016-01-05 17:12:43 -0800

    :green_heart: force push to gh-pages

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cd web && nikola github_deploy 2>&1 | sed -e "s/${GH_TOKEN}/XXXX/g"
+set -e # exit with nonzero if anything fails
+
+# This force pushes build directory and overwrites the gh-pages branch on github
+# destroying all the history.
+cd web/output
+git init
+git add . 
+git commit -m "Travis update"
+git push -f -q "https://${GH_TOKEN}@github.com/${GH_REF}/" master:gh-pages 2>&1 | sed -e "s/${GH_TOKEN}/XXXX/g"
