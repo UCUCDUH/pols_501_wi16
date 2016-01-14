0389d5d430f27ba3c6a300d035fc0c381d1f8bf8
jrnold <jeffrey.arnold@gmail.com>
2016-01-13 16:30:56 -0800

    misc

---
+++ labs/init.R
@@ -0,0 +1,5 @@
+knitr::opt_chunks$set('dev' = 'png', warning = TRUE, error = TRUE,
+                      cache = TRUE, autodep = TRUE,
+                      fig.show = 'all',
+                      echo = TRUE,
+                      results = 'markup')
