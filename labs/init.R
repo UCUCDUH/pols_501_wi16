643de4429d200c9f3767d38f42a23eed2cac042d
jrnold <jeffrey.arnold@gmail.com>
2016-01-13 16:31:26 -0800

    fix knitr init

--- labs/init.R
+++ labs/init.R
@@ -1,4 +1,4 @@
-knitr::opt_chunks$set('dev' = 'png', warning = TRUE, error = TRUE,
+knitr::opts_chunk$set('dev' = 'png', warning = TRUE, error = TRUE,
                       cache = TRUE, autodep = TRUE,
                       fig.show = 'all',
                       echo = TRUE,
